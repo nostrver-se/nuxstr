fdc0b510c896e5427f7005b54f67ca224f411b16
Sebastix <info@sebastix.nl>
2024-04-04 12:52:03 +0200

    add some links on the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,22 @@
 
 <template>
   <div>
-    <h1>Start</h1>
+    <h1>Hello there!</h1>
+    <div>
+      <p>In this demo you can:</p>
+      <ul>
+        <li><NuxtLink to="/login">Login</NuxtLink></li>
+        <li><NuxtLink to="/notes">View notes</NuxtLink></li>
+        <li><NuxtLink to="/long-form-content">View content</NuxtLink></li>
+        <li><NuxtLink to="/places">View places</NuxtLink></li>
+        <li><NuxtLink to="/zap-example">Send a zap</NuxtLink></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+  h1 {
+    @apply text-3xl font-bold
+  }
 </style>
